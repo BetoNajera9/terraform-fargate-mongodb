6ed710c7f507f2d5a03daf38dbf9c9e53a8f79f5
Beto Najera <ing.betonajera@gmail.com>
2025-09-15 18:35:05 -0600

    ✨ feat: add ECS module configuration and associated variables for Terraform

--- modules.tf
+++ modules.tf
@@ -1,6 +1,21 @@
 module "vpc" {
-  source    = "./vpc"
-  vpc_cidr  = var.vpc_vpc_cidr
-  subnets   = var.vpc_subnets
-  az        = var.vpc_az
+  source   = "./vpc"
+  vpc_cidr = var.vpc_vpc_cidr
+  subnets  = var.vpc_subnets
+  az       = var.vpc_az
+}
+
+module "ecs" {
+  source             = "./ecs"
+  cluster_name       = var.ecs_cluster_name
+  task_family        = var.ecs_task_family
+  cpu                = var.ecs_cpu
+  memory             = var.ecs_memory
+  execution_role_arn = var.ecs_execution_role_arn
+  task_role_arn      = var.ecs_task_role_arn
+  container_name     = var.ecs_container_name
+  container_image    = var.ecs_container_image
+  container_port     = var.ecs_container_port
+  subnets            = var.vpc_subnets
+  ecs_sg_id          = module.vpc.vpc_default_sg_id
 }
--- variables.tf
+++ variables.tf
@@ -31,3 +31,64 @@ variable "vpc_az" {
   type        = string
 }
 
+variable "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+}
+
+variable "ecs_task_family" {
+  description = "The family of the ECS task definition"
+  type        = string
+}
+
+variable "ecs_cpu" {
+  description = "The number of CPU units used by the task"
+  type        = string
+}
+
+variable "ecs_memory" {
+  description = "The amount of memory (in MiB) used by the task"
+  type        = string
+}
+
+variable "ecs_execution_role_arn" {
+  description = "The ARN of the task execution role that the Amazon ECS container agent and the Docker daemon can assume"
+  type        = string
+}
+
+variable "ecs_task_role_arn" {
+  description = "The ARN of the IAM role that containers in this task can assume"
+  type        = string
+}
+
+variable "ecs_container_name" {
+  description = "The name of the container"
+  type        = string
+}
+
+variable "ecs_container_image" {
+  description = "The image used to start a container"
+  type        = string
+}
+
+variable "ecs_container_port" {
+  description = "The port number on the container that is bound to the user-specified or automatically assigned host port"
+  type        = number
+}
+
+variable "ecs_desired_count" {
+  description = "The number of instances of the task definition to place and keep running in your service"
+  type        = number
+  default     = 1
+}
+
+variable "ecs_subnets" {
+  description = "A list of subnet IDs associated with the task or service"
+  type        = list(string)
+}
+
+variable "ecs_sg_id" {
+  description = "The ID of the security group to associate with the task or service"
+  type        = string
+}
+
