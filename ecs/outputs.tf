2b2635ab5ad426bac6edc4bcf0b2e118f32c5bcb
Beto Najera <ing.betonajera@gmail.com>
2025-09-19 15:50:24 -0600

    🐛 fix: add descriptions to output variables in ALB, ECS, and VPC configurations

--- ecs/outputs.tf
+++ ecs/outputs.tf
@@ -1,11 +1,14 @@
 output "cluster_id" {
-  value = aws_ecs_cluster.main_ecs.id
+  description = "The ID of the ECS cluster"
+  value       = aws_ecs_cluster.main_ecs.id
 }
 
 output "service_name" {
-  value = aws_ecs_service.ecs_service.name
+  description = "The name of the ECS service"
+  value       = aws_ecs_service.ecs_service.name
 }
 
 output "task_definition_arn" {
-  value = aws_ecs_task_definition.ecs_task.arn
+  description = "The ARN of the ECS task definition"
+  value       = aws_ecs_task_definition.ecs_task.arn
 }
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
-  value = aws_vpc.main_vpc.id
+  description = "The ID of the main VPC"
+  value       = aws_vpc.main_vpc.id
 }
 
 output "public_subnets" {
-  value = aws_subnet.public_subnet.id
+  description = "The IDs of the public subnets"
+  value       = aws_subnet.public_subnet.id
 }
 
 output "private_subnets" {
-  value = aws_subnet.private_subnet.id
+  description = "The IDs of the private subnets"
+  value       = aws_subnet.private_subnet.id
 }
