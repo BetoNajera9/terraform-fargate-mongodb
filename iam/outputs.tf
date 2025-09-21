185f98c1b3ea5cbbe337d628cec92a0a7d888b6b
Beto Najera <ing.betonajera@gmail.com>
2025-09-20 19:13:20 -0600

    ♻️ refactor: rename output variables for IAM and VPC to include resource type prefix

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -1,9 +1,9 @@
-output "ecs_task_execution_role_arn" {
+output "iam_ecs_task_execution_role_arn" {
   description = "ARN del Execution Role de ECS"
   value       = aws_iam_role.ecs_task_execution.arn
 }
 
-output "ecs_task_role_arn" {
+output "iam_ecs_task_role_arn" {
   description = "ARN del Task Role de ECS"
   value       = aws_iam_role.ecs_task_role.arn
 }
