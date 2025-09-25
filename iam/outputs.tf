eaf951d4cfc7d1e8560001c362d5e3aaf81bb8d8
Beto Najera <ing.betonajera@gmail.com>
2025-09-24 20:09:06 -0600

    🐛 fix: update ecs_logs_policy_arn output to use correct IAM role policy reference

--- iam/outputs.tf
+++ iam/outputs.tf
@@ -5,7 +5,7 @@ output "iam_ecs_task_execution_role_arn" {
 
 output "ecs_logs_policy_arn" {
   description = "ARN de la política de logs para ECS"
-  value       = aws_iam_policy.ecs_logs_policy.arn
+  value       = aws_iam_role_policy.ecs_logs_policy.arn
 }
 
 output "lambda_deployment_strategy_role_arn" {
